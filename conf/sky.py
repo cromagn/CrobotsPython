4eb3a3399b0beeb52cf65f52aebaf1686f26e677
cromagn <cromagn@gmail.com>
2020-01-25 02:11:27 +0100

    Create sky.py

---
+++ conf/sky.py
@@ -0,0 +1,7 @@
+class Configuration(object):
+  label = 'sky'
+  matchF2F = 10000
+  match3VS3 = 100
+  match4VS4 = 100
+  sourcePath = '.'
+  listRobots = ['antman', 'bttf','babadook', 'avoider','aswhup']
